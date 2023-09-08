a7a39671d71c1a7b09756d66b727c37f1c4a0c33
Jose A. Morales E <jose.a.morales.e@utexas.edu>
2023-09-08 12:08:38 -0500

    Added Steady State sampler file
    Separate script that simply samples the
    steady-state analytical solution for the WFP equation under a harmonic potential
    and plots the sampled points' distribution together with
    level sets of the known steady state formula.

---
+++ SteadyStateSampler.m
@@ -0,0 +1,49 @@
+Nsamples = 10^4; % <---- PARAMETER 1: VALUE DETERMINES NUMERICS CONVERGENCE
+Nplot=1000;
+xmin=0.; xmax=0.;
+kmin=0.; kmax=0.;
+%ADDITIONAL STEP: Sampling of Steady State (SANITY CHECK).
+% Analytical form of steady state solution is:
+% exp(-x^2/5 - x.k/5 - 3k^2/10)/(2pi*sqrt(5))
+% and for multivariate normal distributions we have the form
+% exp(-(x-mu)Sigma^-1 (x-mu)'/2)/sqrt(|Sigma|(2pi)^d)
+% with Sigma the covariance matrix.
+% In this multivariate form, the steady state solution has a mu=0 and 
+InvSigma = [2./5., 1./5.; 1./5., 3./5.]
+%therefore it's the inverse of
+Sigma = [3., -1,;-1., 2.]
+Sigma*InvSigma
+InvSigma*Sigma
+figure(3); hold on; title('Steady state')
+for i=1:Nsamples
+    RandomVec=mvnrnd([0, 0],Sigma); %Sampling from steady state
+    xss=RandomVec(1);
+    kss=RandomVec(2);
+    scatter(xss,kss,5,"red",'o')
+    if xss<xmin
+	    xmin = xss;
+    end
+    if xss>xmax
+	    xmax = xss;
+    end
+    if kss<kmin
+	    kmin = kss;
+    end
+    if kss>kmax
+	    kmax = kss;
+    end
+end
+
+% LEVEL SETS OF STEADY STATE INCLUDED IN THE FIGURE
+fss=@(x,k) (3./10.)*(k.^2) + (x.^2)/5. + x.*k/5.;
+dxplot = (xmax-xmin)/Nplot;
+dkplot = (kmax-kmin)/Nplot;
+xplot = xmin:dxplot:xmax;
+kplot = kmin:dkplot:kmax;
+[X,K]=meshgrid(xplot,kplot);
+z=fss(X,K);
+
+contour(X,K,z,100);%THIS PROVOKES THE ISSUE...?
+hold off;
+exportgraphics(gcf,'AnalyticalSteadyState.pdf','ContentType','vector')
+%}
