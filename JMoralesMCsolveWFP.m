adacb5f450c26e5b221892f5a70276516d4534c3
Jose A. Morales E <jose.a.morales.e@utexas.edu>
2023-09-19 16:59:20 -0700

    Add files via upload
    Added histograms for both initial condition and numerical solution

--- JMoralesMCsolveWFP.m
+++ JMoralesMCsolveWFP.m
@@ -20,7 +20,7 @@ a=1.0; %h=2*pi; %1=hbar=h/(2pi) => h=2*pi
 sigmax=a/sqrt(2); sigmak=1/(a*sqrt(2));
 
 Nsamples = 10^4; % <---- PARAMETER 1: VALUE DETERMINES NUMERICS CONVERGENCE
-T=10; dt=0.1; % <----- PARAMETERS 2 & 3: VALUES DETERMINE NUMERICAL CVG.
+T=50; dt=0.01; % <----- PARAMETERS 2 & 3: VALUES DETERMINE NUMERICAL CVG.
 Ntime = round(T/dt);
 
 x=zeros(Ntime,Nsamples); k=zeros(Ntime,Nsamples); 
@@ -33,8 +33,9 @@ end
 % PLOT SAMPLING OF INITIAL CONDITION TOGETHER WITH ITS LEVEL SETS
 figure(1); hold on; title('Initial Condition')
 for i=1:Nsamples
-    scatter(x(1,i),k(1,i),5,"red",'o')
+    scatter(x(1,i),k(1,i),5,"red",'.')
 end
+hist = histogram2(x(1,:),k(1,:),'DisplayStyle','tile','ShowEmptyBins','on');
 % LEVEL SETS OF INITIAL CONDITION INCLUDED BELOW
 f=@(x,k) (a^2)*(k.^2) + (x.^2)/(a^2);
 dxplot = (max(x(1,:))-min(x(1,:)))/Nplot;
@@ -64,8 +65,9 @@ end
 %PLOT SAMPLES OF NUMERICAL SOLUTION AND LEVEL SETS OF EXPECTED STEADY STATE
 figure(2); hold on; title('Numerical Solution')
 for i=1:Nsamples
-    scatter(x(Ntime,i),k(Ntime,i),5,"red",'o')
+    scatter(x(Ntime,i),k(Ntime,i),5,"red",'.')
 end
+hist = histogram2(x(Ntime,:),k(Ntime,:),'DisplayStyle','tile','ShowEmptyBins','on');
 % LEVEL SETS OF STEADY STATE (INCLUDED IN THE FIGURE): GAUSSIAN ARGUMENT IS
 fss=@(x,k) (3./10.)*(k.^2) + (x.^2)/5. + x.*k/5.;
 dxplot = (max(x(Ntime,:))-min(x(Ntime,:)))/Nplot;
@@ -77,37 +79,3 @@ z=fss(X,K);
 contour(X,K,z,100)
 hold off;
 exportgraphics(gcf,'NumericalSolution.pdf','ContentType','vector')
-%{
-%ADDITIONAL STEP: Sampling of Steady State (SANITY CHECK).
-% Analytical form of steady state solution is:
-% exp(-x^2/5 - x.k/5 - 3k^2/10)/(2pi*sqrt(5))
-% and for multivariate normal distributions we have the form
-% exp(-(x-mu)Sigma^-1 (x-mu)'/2)/sqrt(|Sigma|(2pi)^d)
-% with Sigma the covariance matrix.
-% In this multivariate form, the steady state solution has a mu=0 and 
-InvSigma = [2./5., 1./5.; 1./5., 3./5.]
-%therefore it's the inverse of
-Sigma = [3., -1,;-1., 2.]
-Sigma*InvSigma
-InvSigma*Sigma
-for i=1:Nsamples
-    RandomVec=mvnrnd([0, 0],Sigma); %Sampling from steady state
-    xss(i)=RandomVec(1);
-    kss(i)=RandomVec(2);
-end
-figure(3); hold on; title('Steady state')
-for i=1:Nsamples
-    scatter(xss(i),kss(i),5,"red",'o')
-end
-% LEVEL SETS OF STEADY STATE INCLUDED IN THE FIGURE
-fss=@(x,k) (3./10.)*(k.^2) + (x.^2)/5. + x.*k/5.;
-dxplot = (max(xss)-min(xss))/Nplot;
-dkplot = (max(kss)-min(kss))/Nplot;
-xplot = min(xss):dxplot:max(xss);
-kplot = min(kss):dkplot:max(kss);
-[X,K]=meshgrid(xss,kss);
-z=fss(X,K);
-contour(X,K,z,100);
-hold off;
-exportgraphics(gcf,'AnalyticalSteadyState.pdf','ContentType','vector')
-%}
