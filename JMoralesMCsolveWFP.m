c311bd75fc0af68b21fcfdbe0db262e88c3a78bb
Jose A. Morales E <jose.a.morales.e@utexas.edu>
2023-11-15 09:07:14 -0800

    Add files via upload
    Code modified to monitor entropy. 
    The first trial has been to apply Shannon's formula to our coherent states for which the Wigner function is non-negative.
    To be modified: use the Husimi function for Wehrl's entropy to be implemented completely.

--- JMoralesMCsolveWFP.m
+++ JMoralesMCsolveWFP.m
@@ -23,8 +23,8 @@ sigmax=a/sqrt(2); sigmak=1/(a*sqrt(2)); %Harmonic oscillator parameters
 Nsamples = 10^4; % <---- PARAMETER 1: VALUE DETERMINES NUMERICS CONVERGENCE
 T=50; dt=0.01; % <----- PARAMETERS 2 & 3: VALUES DETERMINE NUMERICAL CVG.
 Ntime = round(T/dt);
-
-x=zeros(Ntime,Nsamples); k=zeros(Ntime,Nsamples); Entropy=zeros(Ntime,1);
+x=zeros(Ntime,Nsamples); k=zeros(Ntime,Nsamples); 
+Entropy=zeros(Ntime,1); weightL2norm = zeros(Ntime,1);
 Nplot=1000; Sigma = [3., -1,;-1., 2.]
 %STEP 1: Sampling of Initial Condition, represented as point distribution.
 for i=1:Nsamples
@@ -45,6 +45,7 @@ GMModel.mu
 GMModel.Sigma
 CovMatrixIC = GMModel.Sigma
 Entropy(1) = log(det(CovMatrixIC))/2.;
+weightL2norm(1) = sqrt(-1+(sqrt(5)/(sqrt(det(2*CovMatrixIC^(-1)-Sigma^(-1)))*det(CovMatrixIC))))
 gmPDF = @(x,k) arrayfun(@(x0,k0) pdf(GMModel,[x0 k0]),x(1,:)',k(1,:)')
 gfun = gca
 fcontour(gmPDF,[gfun.XLim gfun.YLim],'--r')
@@ -80,6 +81,7 @@ for j=2:Ntime
     GMModel.Sigma
     CovMatrix = GMModel.Sigma
     Entropy(j) = log(det(CovMatrix))/2.;
+    weightL2norm(j) = sqrt(-1+(sqrt(5)/(sqrt(det(2*CovMatrix^(-1)-Sigma^(-1)))*det(CovMatrix))))
 end
 Entropy(Ntime)
 EntropySS = log(5)/2.
@@ -90,6 +92,14 @@ ylabel('Entropy (H)')
 yline(EntropySS)
 title('Entropy (H) vs Time (t)')
 legend({'Entropy of Numerical Solution','Analytical Steady State Entropy'},'Location','best')
+hold off;
+figure(5); hold on;
+plot((1:1:Ntime)*dt,weightL2norm,'.')
+plot((1:1:Ntime)*dt,weightL2norm(1)*exp(-((1:1:Ntime)*dt)*(1-1/sqrt(5))/2),'r')
+xlabel('Time (t)')
+ylabel('L2mu')
+legend({'Weighted L2 distance to Steady State','Exponential Decay Rate Bound'},'Location','best')
+title('Weighted L2 distance to Steady state (L2mu)')
 hold off;
 %PLOT SAMPLES OF NUMERICAL SOLUTION AND LEVEL SETS OF EXPECTED STEADY STATE
 figure(2); hold on; title('Numerical Solution')
